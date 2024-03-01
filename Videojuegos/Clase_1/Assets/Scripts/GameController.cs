c9cd046b2f55e29711aeff5412c7b13edc0106fd
A01027920 <A01027920@tec.mx>
2024-03-01 17:35:28 -0600

    Pong

--- Videojuegos/Clase_1/Assets/Scripts/GameController.cs
+++ Videojuegos/Clase_1/Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ Keep track of scores and ball status
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
@@ -17,6 +18,11 @@ public class GameController : MonoBehaviour
 
     public int pointsLeft = 0;
     public int pointsRight = 0;
+
+    public TMP_Text leftScore;
+    public TMP_Text rightScore;
+
+    public int maxPoints;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,20 +37,34 @@ public class GameController : MonoBehaviour
             StartGame();
         }
     }
-
-    void StartGame()
-    {
+    public void Reset(){
+        Destroy(dot);
         dot = Instantiate(dotPrefab);
         //Vector3 direction = new Vector3(Random.value) hay una manera de hacer esto más locos
         dot.GetComponent<Rigidbody2D>().velocity = Random.onUnitSphere * force;
+    }
+
+    void StartGame()
+    {
+        pointsLeft = 0;
+        pointsRight = 0;
+        leftScore.text = pointsLeft.ToString();
+        rightScore.text = pointsRight.ToString();
+        Reset();
     }
 
     public void AddPoints(int side)
     {
         if(side == 1){
             pointsLeft++;
+            if (pointsLeft >= maxPoints){
+                //UnityEngine.SceneManagement.SceneManager.LoadScene("Victory"); para llevarte a otra escena
+                rightScore.text = "Winner!" + pointsRight.ToString();
+            }
+            leftScore.text = pointsLeft.ToString();
         } else {
             pointsRight++;
+            rightScore.text = pointsRight.ToString();
         }
         Destroy(dot);
         StartGame();
